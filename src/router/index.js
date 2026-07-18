8c3d634d00951be07a593f75f4bfc776f5ebc82e
kphan108205-svg <kphan108205@gmail.com>
2026-07-18 22:28:48 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -8,10 +8,16 @@ const routes = [
     component: ContactBook,
   },
   {
-	path: "/:pathMatch(.*)*",
-	name: "notfound",
-	component: () => import("@/views/NotFound.vue"),
-  }
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true,
+  },
 ];
 
 const router = createRouter({
